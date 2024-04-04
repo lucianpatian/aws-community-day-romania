197071f821379813fea543b8afe326fa150eceb8
Johannes Grumboeck <johannes.grumboeck@redbull.com>
2024-04-04 09:03:10 +0200

    feat(volunteer): add some more questions and answers

--- src/pages/volunteer.jsx
+++ src/pages/volunteer.jsx
@@ -4,6 +4,7 @@ import React from 'react';
 import Volunteer from 'components/pages/volunteer/volunteer';
 import FAQ from 'components/shared/faq';
 import Layout from 'components/shared/layout';
+import Link from 'components/shared/link/link';
 import SEO from 'components/shared/seo';
 import SEO_DATA from 'constants/seo-data';
 
@@ -11,7 +12,34 @@ const VolunteerPage = () => (
   <Layout>
     <Volunteer />
     <br />
-    <FAQ question="Foobar" answer="is a test" />
+    <FAQ
+      question="What are the requirements for volunteers?"
+      answer="To be a volunteer at KCD Austria, you must be at least 18 years old.
+      You should also have good English skills (at least B2 level) and be motivated.
+      Depending on the area of application, you should also be able to remain calm in stressful situations and be good at interacting with people."
+    />
+    <br />
+    <FAQ
+      question="What are the benefits of volunteering?"
+      answer="Every volunteer who helps out will get free entrance to both conference days."
+    />
+    <br />
+    <FAQ
+      question="Can I see anything from KCD Austria as a volunteer?"
+      answer="Of course! We will make the schedule flexible so that you can also attend some of the talks."
+    />
+    <br />
+    <FAQ
+      question="Do you have another question?"
+      answer={(
+        <>
+          Feel free to contact us directly by sending an email to {' '}
+          <Link to='mailto:organizers@kcdaustria.at' ariaLabel='organizers@kcdaustria.at'>
+            organizers@kcdaustria.at
+          </Link>.
+        </>
+      )}
+    />
   </Layout>
 );
 
