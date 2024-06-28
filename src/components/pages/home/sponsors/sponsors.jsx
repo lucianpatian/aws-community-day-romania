a314dc9191b6862ee55fecc5e985b7fa10e827e7
Lucian Patian <patianl@haufe.com>
2024-06-28 15:58:54 +0300

    updated sponsors

--- src/components/pages/home/sponsors/sponsors.jsx
+++ src/components/pages/home/sponsors/sponsors.jsx
@@ -2,90 +2,90 @@ import React from 'react';
 
 import Button from 'components/shared/button';
 
-import Whizus from './logos/Whizus.png';
-import AwsCommunityDays from './logos/AwsCommunityDays.png';
-import Cncf from './logos/Cncf.png';
-import KcdMunich from './logos/KcdMunich.svg';
-import KcdZurich from './logos/KcdZurich.png';
-import MeetingDestinationVienna from './logos/MeetingDestinationVienna.svg';
-import KubeCareers from './logos/KubeCareers.webp';
-import KubeEvents from './logos/KubeEvents.webp';
-import RaiffeisenBankInternational from './logos/RaiffeisenBankInternational.png';
-import FullStacks from './logos/FullStacks.png';
-import Cloudpilots from './logos/Cloudpilots.png';
-import CloudWerkstatt from './logos/CloudWerkstatt.png';
-import NextLayer from './logos/NextLayer.webp';
-import NTS from './logos/NTS.png';
-import Exoscale from './logos/Exoscale.png';
-import Dynatrace from './logos/Dynatrace.png';
-import Splunk from './logos/Splunk.png';
-import Tigera from './logos/Tigera.png';
-import PorscheInformatik from './logos/PorscheInformatik.png';
-import Veeam from './logos/Veeam.png';
+import Haufe Group from './logos/HG_Logo_Standard.jpg';
+// import AwsCommunityDays from './logos/AwsCommunityDays.png';
+// import Cncf from './logos/Cncf.png';
+// import KcdMunich from './logos/KcdMunich.svg';
+// import KcdZurich from './logos/KcdZurich.png';
+// import MeetingDestinationVienna from './logos/MeetingDestinationVienna.svg';
+// import KubeCareers from './logos/KubeCareers.webp';
+// import KubeEvents from './logos/KubeEvents.webp';
+// import RaiffeisenBankInternational from './logos/RaiffeisenBankInternational.png';
+// import FullStacks from './logos/FullStacks.png';
+// import Cloudpilots from './logos/Cloudpilots.png';
+// import CloudWerkstatt from './logos/CloudWerkstatt.png';
+// import NextLayer from './logos/NextLayer.webp';
+// import NTS from './logos/NTS.png';
+// import Exoscale from './logos/Exoscale.png';
+// import Dynatrace from './logos/Dynatrace.png';
+// import Splunk from './logos/Splunk.png';
+// import Tigera from './logos/Tigera.png';
+// import PorscheInformatik from './logos/PorscheInformatik.png';
+// import Veeam from './logos/Veeam.png';
 
-const sponsors = [
-  {
-    title: 'Gold',
-    sponsors: [
-      { url: 'https://www.whizus.com', logo: Whizus },
-      { url: 'https://www.cloudpilots.com', logo: Cloudpilots },
-      { url: 'https://www.exoscale.com/', logo: Exoscale },
-      { url: 'https://www.dynatrace.com/', logo: Dynatrace },
-      { url: 'https://www.splunk.com/', logo: Splunk }
-    ],
-  },
-  {
-    title: 'Silver',
-    sponsors: [
-      { url: 'https://rbinternational.com', logo: RaiffeisenBankInternational },
-      { url: 'https://www.fullstacks.eu', logo: FullStacks },
-      { url: 'https://www.cloudwerkstatt.com', logo: CloudWerkstatt },
-      { url: 'https://www.nextlayer.at', logo: NextLayer },
-      { url: 'https://www.nts.eu', logo: NTS },
-      { url: 'https://www.tigera.io', logo: Tigera },
-      { url: 'https://www.veeam.com', logo: Veeam }
-    ],
-  },
-  {
-    title: 'Community',
-    sponsors: [
-      { url: 'https://www.porscheinformatik.com', logo: PorscheInformatik }
-    ],
-  },
-  {
-    title: 'Media Partners',
-    sponsors: [
-      { url: 'https://www.cncf.io', logo: Cncf },
-      { url: 'https://www.kcdmunich.de', logo: KcdMunich },
-      { url: 'https://kcdzurich.ch', logo: KcdZurich },
-      { url: 'https://aws-community-day.de', logo: AwsCommunityDays, background: '#2A3644' },
-      { url: 'https://meeting.vienna.info/en', logo: MeetingDestinationVienna },
-      { url: 'https://kube.careers', logo: KubeCareers },
-      { url: 'https://kube.events', logo: KubeEvents },
-    ],
-  },
-];
+// const sponsors = [
+//   {
+//     title: 'Gold',
+//     sponsors: [
+//       { url: 'https://www.whizus.com', logo: Whizus },
+//       { url: 'https://www.cloudpilots.com', logo: Cloudpilots },
+//       { url: 'https://www.exoscale.com/', logo: Exoscale },
+//       { url: 'https://www.dynatrace.com/', logo: Dynatrace },
+//       { url: 'https://www.splunk.com/', logo: Splunk }
+//     ],
+//   },
+//   {
+//     title: 'Silver',
+//     sponsors: [
+//       { url: 'https://rbinternational.com', logo: RaiffeisenBankInternational },
+//       { url: 'https://www.fullstacks.eu', logo: FullStacks },
+//       { url: 'https://www.cloudwerkstatt.com', logo: CloudWerkstatt },
+//       { url: 'https://www.nextlayer.at', logo: NextLayer },
+//       { url: 'https://www.nts.eu', logo: NTS },
+//       { url: 'https://www.tigera.io', logo: Tigera },
+//       { url: 'https://www.veeam.com', logo: Veeam }
+//     ],
+//   },
+//   {
+//     title: 'Community',
+//     sponsors: [
+//       { url: 'https://www.porscheinformatik.com', logo: PorscheInformatik }
+//     ],
+//   },
+//   {
+//     title: 'Media Partners',
+//     sponsors: [
+//       { url: 'https://www.cncf.io', logo: Cncf },
+//       { url: 'https://www.kcdmunich.de', logo: KcdMunich },
+//       { url: 'https://kcdzurich.ch', logo: KcdZurich },
+//       { url: 'https://aws-community-day.de', logo: AwsCommunityDays, background: '#2A3644' },
+//       { url: 'https://meeting.vienna.info/en', logo: MeetingDestinationVienna },
+//       { url: 'https://kube.careers', logo: KubeCareers },
+//       { url: 'https://kube.events', logo: KubeEvents },
+//     ],
+//   },
+// ];
 
 const Sponsors = () => (
   <section className="safe-paddings container-md text-center">
     <h1 id="sponsors">Sponsors</h1>
 
     <p>
-      Join us in making KCD Vienna a memorable and impactful event for all attendees. To explore
+      Join us in making AWS Community Day Romania a memorable and impactful event for all attendees. To explore
       sponsorship opportunities, please don't hesitate to get in touch with us today. Your support
       will help us create an unforgettable experience for the community while enhancing your brand's
       visibility and recognition. Contact us at{' '}
       <a
-        href="mailto:organizers@kcdaustria.at"
+        href="mailto:contact@aws-community.ro"
         className="text-purple"
         target="_blank"
         rel="noreferrer"
       >
         {' '}
-        organizers@kcdaustria.at{' '}
+        contact@aws-community.ro{' '}
       </a>
     </p>
-    <div className="mt-6 mb-12 flex flex-row justify-center gap-4 sm:flex-col">
+    {/* <div className="mt-6 mb-12 flex flex-row justify-center gap-4 sm:flex-col">
       <a
         href="https://drive.google.com/file/d/15cC_G7hZrMVhrIwiukkfxPvMoRvBPKKZ/view?usp=sharing"
         target="_blank"
@@ -96,7 +96,7 @@ const Sponsors = () => (
       <a href="mailto:organizers@kcdaustria.at" target="_blank" rel="noreferrer" aria-label="Get in touch">
         <Button>Get in touch</Button>
       </a>
-    </div>
+    </div> */}
 
     {sponsors.map(({ title, sponsors }, index) => {
       // Randomly sort the sponsors array
