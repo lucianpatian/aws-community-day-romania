a68f64191cae5b2c2a13fa28b61a2810bc694ef1
Adrian Bere <adibere@amazon.com>
2024-07-02 09:53:08 +0300

    First attempt for agenda

--- src/components/pages/home/schedule/schedule.jsx
+++ src/components/pages/home/schedule/schedule.jsx
@@ -1,35 +1,106 @@
 import React from 'react';
 
 const Schedule = () => (
-    <section className="safe-paddings container-md text-center">
+    <section className="safe-paddings container-md">
         <h1 id="schedule">Schedule</h1>
-        <p>Will be available after the Call for Proposals ends</p>
-        <div className="flex flex-row md:flex-col gap-4">
-            {/* <div
-                className="w-1/3 md:w-full bg-gray-12 rounded-md p-4 flex-col inline-block justify-center content-center">
-                <strong className="text-sm bg-orange py-1 px-2 rounded-full">Tuesday, October 8</strong>
-                <h1 className="p-0 m-0 mt-4 mb-2 text-2xl">Workshops</h1>
-                <p className="text-center">
-                    Dive into whole day of hands-on workshops from the community. <br/> Learn, connect, and have fun!
+        <p className="text-center">Will be available after the Call for Proposals ends</p><br />
+        <div className="flex flex-col gap-4">
+            <div
+                className="w-full bg-blue-3 rounded-md p-4 flex-col">
+                <strong className="text-sm bg-orange py-1 px-2 rounded-full">09:00 - 10:00</strong>
+                <h1 className="p-0 m-0 mt-4 mb-2 text-lg text-left">Welcome & Registration</h1>
+                <p className="text-left text-base">
+                    Let's connect, and have fun!
                 </p>
             </div>
             <div
-                className="w-1/3 md:w-full bg-gray-12 rounded-md p-4 flex-col inline-block justify-center content-center">
-                <strong className="text-sm bg-pink text-white py-1 px-2 rounded-full">Wednesday, October 9</strong>
-                <h1 className="p-0 m-0 mt-4 mb-2 text-2xl">Conference</h1>
-                <p className="text-center">
-                    Let's kick-off the main event with inspiring talks and networking opportunities. Get ready to be
-                    wowed!
+                className="w-full bg-blue-3 rounded-md p-4 flex-col">
+                <strong className="text-sm bg-orange text-white py-1 px-2 rounded-full">10:00 - 10:30</strong>
+                <h1 className="p-0 m-0 mt-4 mb-2 text-lg text-left">Keynotes</h1>
+                <p className="text-left text-base">
+                    Let's kick-off the main event with inspiring keynotes.
                 </p>
             </div>
             <div
-                className="w-1/3 md:w-full bg-gray-12 rounded-md p-4 flex-col inline-block justify-center content-center">
-                <strong className="text-sm bg-purple text-white py-1 px-2 rounded-full">Thursday, October 10</strong>
-                <h1 className="p-0 m-0 mt-4 mb-2 text-2xl">Conference</h1>
-                <p className="text-center">
-                    Wrap up with even more insights, deeper conversations, and lasting connections.
+                className="w-full bg-blue-3 rounded-md p-4 flex-col">
+                <strong className="text-sm bg-orange text-white py-1 px-2 rounded-full">10:35 - 11:00</strong>
+                <h1 className="p-0 m-0 mt-4 mb-2 text-lg text-left">Talk 1</h1>
+                <p className="text-left text-base">
+                    TBD
                 </p>
-            </div> */}
+            </div>
+            <div
+                className="w-full bg-blue-3 rounded-md p-4 flex-col">
+                <strong className="text-sm bg-orange text-white py-1 px-2 rounded-full">11:05 - 11:30</strong>
+                <h1 className="p-0 m-0 mt-4 mb-2 text-lg text-left">Talk 2</h1>
+                <p className="text-left text-base">
+                    TBD
+                </p>
+            </div>
+            <div
+                className="w-full bg-blue-3 rounded-md p-4 flex-col">
+                <strong className="text-sm bg-orange text-white py-1 px-2 rounded-full">11:35 - 12:00</strong>
+                <h1 className="p-0 m-0 mt-4 mb-2 text-lg text-left">Talk 3</h1>
+                <p className="text-left text-base">
+                    TBD
+                </p>
+            </div>
+            <div
+                className="w-full bg-blue-3 rounded-md p-4 flex-col">
+                <strong className="text-sm bg-orange text-white py-1 px-2 rounded-full">12:00 - 13:30</strong>
+                <h1 className="p-0 m-0 mt-4 mb-2 text-lg text-left">Lunch</h1>
+                <p className="text-left text-base">
+                    Take a lunch break and relax!
+                </p>
+            </div>
+            <div
+                className="w-full bg-blue-3 rounded-md p-4 flex-col">
+                <strong className="text-sm bg-orange text-white py-1 px-2 rounded-full">13:30 - 14:50</strong>
+                <h1 className="p-0 m-0 mt-4 mb-2 text-lg text-left">Discussion Panel - moderator Raul Geana</h1>
+                <p className="text-left text-base">
+                    Panel
+                </p>
+            </div>
+            <div
+                className="w-full bg-blue-3 rounded-md p-4 flex-col">
+                <strong className="text-sm bg-orange text-white py-1 px-2 rounded-full">14:50 - 15:30</strong>
+                <h1 className="p-0 m-0 mt-4 mb-2 text-lg text-left">Coffee Break</h1>
+                <p className="text-left text-base">
+                    Take a coffee break and relax!
+                </p>
+            </div>
+            <div
+                className="w-full bg-blue-3 rounded-md p-4 flex-col">
+                <strong className="text-sm bg-orange text-white py-1 px-2 rounded-full">15:30 - 16:15</strong>
+                <h1 className="p-0 m-0 mt-4 mb-2 text-lg text-left">Talk 4</h1>
+                <p className="text-left text-base">
+                    TBD
+                </p>
+            </div>
+            <div
+                className="w-full bg-blue-3 rounded-md p-4 flex-col">
+                <strong className="text-sm bg-orange text-white py-1 px-2 rounded-full">16:20 - 16:45</strong>
+                <h1 className="p-0 m-0 mt-4 mb-2 text-lg text-left">Talk 5</h1>
+                <p className="text-left text-base">
+                    TBD
+                </p>
+            </div>
+            <div
+                className="w-full bg-blue-3 rounded-md p-4 flex-col">
+                <strong className="text-sm bg-orange text-white py-1 px-2 rounded-full">16:50 - 17:15</strong>
+                <h1 className="p-0 m-0 mt-4 mb-2 text-lg text-left">Talk 6</h1>
+                <p className="text-left text-base">
+                    TBD
+                </p>
+            </div>
+            <div
+                className="w-full bg-blue-3 rounded-md p-4 flex-col">
+                <strong className="text-sm bg-orange text-white py-1 px-2 rounded-full">17:15 - 17:30</strong>
+                <h1 className="p-0 m-0 mt-4 mb-2 text-lg text-left">Closing talk</h1>
+                <p className="text-left text-base">
+                    Hope you enjoyed the experience. See you next time !
+                </p>
+            </div>
         </div>
     </section>
 );
