380f0c69e8cd5627b660d8208b4221f2f6762ddd
Adrian Bere <adibere@amazon.com>
2024-07-02 08:10:01 +0300

    this looks bad on mobile device

--- src/components/pages/home/info/info.jsx
+++ src/components/pages/home/info/info.jsx
@@ -13,14 +13,14 @@ const Info = () => (
 
     <h1>Why attend and what to expect?</h1>
     <div className="flex flex-row md:flex-col">
-      <div className="w-1/3 md:w/full text-center px-4">
+      <div className="w-1/3 md:w-full text-center px-4">
         <h2 className="text-orange">10+</h2>
         <p>
           <strong>Technically competent talks & panels</strong> packed with insights that will fuel your professional growth.
           Explore AWS services and learn how to implement them in real-world scenarios.
         </p>
       </div>
-      <div className="w-1/3 md:w/full text-center px-4">
+      <div className="w-1/3 md:w-full text-center px-4">
         <h2 className="text-orange">ONE</h2>
         <p>
           <strong>Stage</strong> in an exceptional venue offering enough space for{' '}
@@ -29,7 +29,7 @@ const Info = () => (
           {/* <strong>workshops</strong>. */}
         </p>
       </div>
-      <div className="w-1/3 md:w/full text-center px-4">
+      <div className="w-1/3 md:w-full text-center px-4">
         <h2 className="text-orange">250+</h2>
         <p>
           <strong>Diverse attendees & speakers</strong> including <strong>leading experts</strong>{' '}
