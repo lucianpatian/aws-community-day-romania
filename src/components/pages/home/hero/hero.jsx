d6732843e244da6b532e3cc443d3c8bdae96a98e
Lucian Patian <patianl@haufe.com>
2024-07-04 14:47:59 +0200

    Revert "Revert "change AWS User Groups name""
    This reverts commit e3bf79fdd1c414eb6ae5c61d0174ca3b216021c3.

--- src/components/pages/home/hero/hero.jsx
+++ src/components/pages/home/hero/hero.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {isMobile} from 'react-device-detect';
+import { isMobile } from 'react-device-detect';
 
 import illustration from './images/CommunityDay_Sitemerch_hexagon01.png';
 import Button from '../../../shared/button';
@@ -9,12 +9,12 @@ const Hero = () => (
   <section className="safe-paddings overflow-hidden mt-12 mb-16">
     <div className="container w-full flex flex-row items-center md:flex-col">
       <div className="flex flex-col flex-grow flex-shrink w-1/2 md:w-full pr-[48px] md:pr-0">
-      <img className="w-260" src={LogoFirstEdition} alt="LogoAWS" />
+        <img className="w-260" src={LogoFirstEdition} alt="LogoAWS" />
         <p className="mt-8 w-full">
           On <strong>October 3rd</strong> the first AWS Community Day Romania will gather in Timisoara at  {' '}
-          <a 
-            href="https://maps.app.goo.gl/pRAArsxsin8L3hUp9" 
-            target="_blank" 
+          <a
+            href="https://maps.app.goo.gl/pRAArsxsin8L3hUp9"
+            target="_blank"
             rel="noopener noreferrer"
           >
             Iulius Congress Hall
@@ -24,7 +24,7 @@ const Hero = () => (
           Join us for a one day technical event loaded with exciting talks and
           networking opportunities. The event is aimed at developers, platform people and other
           IT professionals interested in cloud native technologies while being
-          supported by all AWS Usergroups from Romania.
+          supported by all AWS User Groups from Romania.
         </p>
         <div className="mt-10 flex container flex-row gap-4 w-full sm:flex-col p-0">
           <Button onClick={(e) => {
