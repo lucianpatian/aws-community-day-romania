78d13147cdd2e7f6fc5d51ed7070117f746bb1dd
Lucian Patian <patianl@haufe.com>
2024-07-04 14:47:51 +0200

    Revert "Revert "agenda polish""
    This reverts commit f11c773a2f9f588dfa901b2170874ef009c90a37.

--- src/components/pages/home/schedule/schedule.jsx
+++ src/components/pages/home/schedule/schedule.jsx
@@ -3,67 +3,67 @@ import React from 'react';
 const Schedule = () => (
     <section className="safe-paddings container-md">
         <h1 id="schedule">Schedule</h1>
-        <p className="text-center">Will be available after the Call for Proposals ends</p><br />
+        <p className="text-center">All details will be available after the Call for Proposals ends</p><br />
         <div className="flex flex-col gap-4">
             <div
                 className="w-full bg-blue-3 rounded-md p-4 flex-col">
-                <strong className="text-sm bg-orange py-1 px-2 rounded-full">09:00 - 10:00</strong>
+                <strong className="text-sm bg-purple py-1 px-2 rounded-full">09:00 - 10:00</strong>
                 <h1 className="p-0 m-0 mt-4 mb-2 text-lg text-left">Welcome & Registration</h1>
                 <p className="text-left text-base">
-                    Let's connect, and have fun!
+                    Get your badge and a coffee to start the day right! Make sure you wear something comfy.
                 </p>
             </div>
             <div
                 className="w-full bg-blue-3 rounded-md p-4 flex-col">
-                <strong className="text-sm bg-orange text-white py-1 px-2 rounded-full">10:00 - 10:30</strong>
+                <strong className="text-sm bg-purple text-white py-1 px-2 rounded-full">10:00 - 10:30</strong>
                 <h1 className="p-0 m-0 mt-4 mb-2 text-lg text-left">Keynotes</h1>
                 <p className="text-left text-base">
-                    Let's kick-off the main event with inspiring keynotes.
+                    Time for the welcome talk and schedule presentation.
                 </p>
             </div>
             <div
                 className="w-full bg-blue-3 rounded-md p-4 flex-col">
-                <strong className="text-sm bg-orange text-white py-1 px-2 rounded-full">10:35 - 11:00</strong>
+                <strong className="text-sm bg-purple text-white py-1 px-2 rounded-full">10:35 - 11:00</strong>
                 <h1 className="p-0 m-0 mt-4 mb-2 text-lg text-left">Talk 1</h1>
                 <p className="text-left text-base">
-                    TBD
+                Time for the first talk.
                 </p>
             </div>
             <div
                 className="w-full bg-blue-3 rounded-md p-4 flex-col">
-                <strong className="text-sm bg-orange text-white py-1 px-2 rounded-full">11:05 - 11:30</strong>
+                <strong className="text-sm bg-purple text-white py-1 px-2 rounded-full">11:05 - 11:30</strong>
                 <h1 className="p-0 m-0 mt-4 mb-2 text-lg text-left">Talk 2</h1>
                 <p className="text-left text-base">
-                    TBD
+                Time for the second talk.
                 </p>
             </div>
             <div
                 className="w-full bg-blue-3 rounded-md p-4 flex-col">
-                <strong className="text-sm bg-orange text-white py-1 px-2 rounded-full">11:35 - 12:00</strong>
+                <strong className="text-sm bg-purple text-white py-1 px-2 rounded-full">11:35 - 12:00</strong>
                 <h1 className="p-0 m-0 mt-4 mb-2 text-lg text-left">Talk 3</h1>
                 <p className="text-left text-base">
-                    TBD
+                Time for the third talk.
                 </p>
             </div>
             <div
                 className="w-full bg-blue-3 rounded-md p-4 flex-col">
-                <strong className="text-sm bg-orange text-white py-1 px-2 rounded-full">12:00 - 13:30</strong>
+                <strong className="text-sm bg-purple text-white py-1 px-2 rounded-full">12:00 - 13:30</strong>
                 <h1 className="p-0 m-0 mt-4 mb-2 text-lg text-left">Lunch</h1>
                 <p className="text-left text-base">
-                    Take a lunch break and relax!
+                    Enjoy something tasty and get back with lots of energy!
                 </p>
             </div>
             <div
                 className="w-full bg-blue-3 rounded-md p-4 flex-col">
-                <strong className="text-sm bg-orange text-white py-1 px-2 rounded-full">13:30 - 14:50</strong>
+                <strong className="text-sm bg-purple text-white py-1 px-2 rounded-full">13:30 - 14:50</strong>
                 <h1 className="p-0 m-0 mt-4 mb-2 text-lg text-left">Discussion Panel - moderator Raul Geana</h1>
                 <p className="text-left text-base">
-                    Panel
+                Time for the panel talks.
                 </p>
             </div>
             <div
                 className="w-full bg-blue-3 rounded-md p-4 flex-col">
-                <strong className="text-sm bg-orange text-white py-1 px-2 rounded-full">14:50 - 15:30</strong>
+                <strong className="text-sm bg-purple text-white py-1 px-2 rounded-full">14:50 - 15:30</strong>
                 <h1 className="p-0 m-0 mt-4 mb-2 text-lg text-left">Coffee Break</h1>
                 <p className="text-left text-base">
                     Take a coffee break and relax!
@@ -71,34 +71,34 @@ const Schedule = () => (
             </div>
             <div
                 className="w-full bg-blue-3 rounded-md p-4 flex-col">
-                <strong className="text-sm bg-orange text-white py-1 px-2 rounded-full">15:30 - 16:15</strong>
+                <strong className="text-sm bg-purple text-white py-1 px-2 rounded-full">15:30 - 16:15</strong>
                 <h1 className="p-0 m-0 mt-4 mb-2 text-lg text-left">Talk 4</h1>
                 <p className="text-left text-base">
-                    TBD
+                Time for the fourth talk.
                 </p>
             </div>
             <div
                 className="w-full bg-blue-3 rounded-md p-4 flex-col">
-                <strong className="text-sm bg-orange text-white py-1 px-2 rounded-full">16:20 - 16:45</strong>
+                <strong className="text-sm bg-purple text-white py-1 px-2 rounded-full">16:20 - 16:45</strong>
                 <h1 className="p-0 m-0 mt-4 mb-2 text-lg text-left">Talk 5</h1>
                 <p className="text-left text-base">
-                    TBD
+                Time for the fifth talk.
                 </p>
             </div>
             <div
                 className="w-full bg-blue-3 rounded-md p-4 flex-col">
-                <strong className="text-sm bg-orange text-white py-1 px-2 rounded-full">16:50 - 17:15</strong>
+                <strong className="text-sm bg-purple text-white py-1 px-2 rounded-full">16:50 - 17:15</strong>
                 <h1 className="p-0 m-0 mt-4 mb-2 text-lg text-left">Talk 6</h1>
                 <p className="text-left text-base">
-                    TBD
+                Time for the sixth talk.
                 </p>
             </div>
             <div
                 className="w-full bg-blue-3 rounded-md p-4 flex-col">
-                <strong className="text-sm bg-orange text-white py-1 px-2 rounded-full">17:15 - 17:30</strong>
+                <strong className="text-sm bg-purple text-white py-1 px-2 rounded-full">17:15 - 17:30</strong>
                 <h1 className="p-0 m-0 mt-4 mb-2 text-lg text-left">Closing talk</h1>
                 <p className="text-left text-base">
-                    Hope you enjoyed the experience. See you next time !
+                Time for the closing talk. We hope you enjoyed the experience. See you next time!
                 </p>
             </div>
         </div>
